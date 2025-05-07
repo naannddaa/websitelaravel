f3a35bbda93dd7a11458cbcb025c93ad97672036
developer <rikaaarika37@gmail.com>
2025-05-07 23:50:34 +0700

    api diajukan edited

--- app/Http/Controllers/API/status_diajukan_controller.php
+++ app/Http/Controllers/API/status_diajukan_controller.php
@@ -10,30 +10,27 @@ use Illuminate\Support\Facades\DB;
 class status_diajukan_controller extends Controller
 {
     public function index(Request $request)
-    {
-       $nik = $request->query('nik');
+{
+    $nik = $request->query('nik');
 
-        if (!$nik) {
-            return response()->json([
-                'error' => 'Parameter NIK tidak ditemukan'
-            ], 400);
-        }
-
-        $data = DB::table('master_pengajuan')
-            ->join('master_surat', 'master_pengajuan.id_surat', '=', 'master_surat.id_surat')
-            ->select(
-                'master_surat.nama_surat',
-                'master_pengajuan.tanggal_diajukan',
-                'master_pengajuan.status'
-            )
-            ->where('master_pengajuan.nik', $nik)
-            ->where(function($query) {
-            $query->where('master_pengajuan.status', 'Diajukan')
-                  ->orWhere('master_pengajuan.status', 'Disetujui RT')
-                  ->orWhere('master_pengajuan.status', 'Disetujui RW');
-            })
-            ->get();
-
-        return response()->json($data);
+    if (!$nik) {
+        return response()->json([
+            'error' => 'Parameter NIK tidak ditemukan'
+        ], 400);
     }
+
+    $data = DB::table('master_pengajuan')
+        ->join('master_surat', 'master_pengajuan.id_surat', '=', 'master_surat.id_surat')
+        ->select(
+            'master_surat.nama_surat',
+            'master_pengajuan.tanggal_diajukan',
+            'master_pengajuan.status'
+        )
+        ->where('master_pengajuan.nik', $nik)
+        ->where('master_pengajuan.status', 'disetujui rt') // <== hanya status ini
+        ->get();
+
+    return response()->json($data);
+}
+
 }
